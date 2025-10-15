811b2375e6569620c7cdab1d7b88f69b9664e255
Anatoly Karlov <karleowne@gmail.com>
2025-10-15 16:32:27 +0700

    fix TraceContextExtractor

--- src/main/java/dev/vality/woody/http/bridge/tracing/TraceContextExtractor.java
+++ src/main/java/dev/vality/woody/http/bridge/tracing/TraceContextExtractor.java
@@ -1,12 +1,18 @@
 package dev.vality.woody.http.bridge.tracing;
 
+import dev.vality.woody.api.flow.WFlow;
+import dev.vality.woody.api.generator.IdGenerator;
+import dev.vality.woody.api.trace.TraceData;
 import dev.vality.woody.api.trace.context.TraceContext;
+import dev.vality.woody.http.bridge.token.TokenPayload;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.context.propagation.TextMapSetter;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -16,7 +22,7 @@ import static dev.vality.woody.http.bridge.tracing.TraceHeadersConstants.*;
 
 @Slf4j
 @UtilityClass
-public class TraceContextHeadersExtractor {
+public class TraceContextExtractor {
 
     public Map<String, String> extractHeaders() {
         var traceData = Objects.requireNonNull(TraceContext.getCurrentTraceData(),
@@ -44,11 +50,60 @@ public class TraceContextHeadersExtractor {
         return headers;
     }
 
+    public TokenPayload extractTokenPayload(String termUrl,
+                                            String invoiceFormatPaymentId) {
+        return extractTokenPayload(termUrl, invoiceFormatPaymentId, LocalDateTime.now(ZoneOffset.UTC));
+    }
+
+    public TokenPayload extractTokenPayload(String termUrl,
+                                            String invoiceFormatPaymentId,
+                                            LocalDateTime timestamp) {
+        var traceData = Objects.requireNonNull(TraceContext.getCurrentTraceData(),
+                "TraceData should be present in TraceContext");
+        var otelSpan = Objects.requireNonNull(traceData.getOtelSpan(),
+                "OTel span should be attached to TraceData");
+        if (!otelSpan.getSpanContext().isValid()) {
+            throw new IllegalStateException("SpanContext must be valid");
+        }
+
+        var span = traceData.getActiveSpan().getSpan();
+        var traceId = span.getTraceId();
+        var spanId = span.getId();
+
+        var carrier = new HashMap<String, String>();
+        GlobalOpenTelemetry.getPropagators()
+                .getTextMapPropagator()
+                .inject(traceData.getOtelContext(), carrier, MAP_SETTER);
+        var traceparent = carrier.get(OTEL_TRACE_PARENT);
+        var tracestate = carrier.get(OTEL_TRACE_STATE);
+
+        return new TokenPayload(
+                termUrl,
+                timestamp != null ? timestamp : LocalDateTime.now(ZoneOffset.UTC),
+                invoiceFormatPaymentId,
+                traceId,
+                spanId,
+                generateSpanId(traceData),
+                traceparent,
+                tracestate
+        );
+    }
+
     private void putIfNotNull(Map<String, String> headers,
                               String key,
                               String value) {
         if (value != null && !value.isEmpty()) {
             headers.put(key, value);
+        }
+    }
+
+    private String generateSpanId(TraceData traceData) {
+        var root = traceData.isRoot();
+        var defaultIdGenerator = WFlow.createDefaultIdGenerator();
+        if (root) {
+            return defaultIdGenerator.generateId();
+        } else {
+            return defaultIdGenerator.generateId("", traceData.getServiceSpan().getCounter().incrementAndGet());
         }
     }
 
